fc59216d01725d75145efc71457ab64f02af09b1
TruptiSahu <truptiranjansahu2@gmail.com>
2020-11-09 21:04:20 -0800

    Dome

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-const allClearButton = document.querySelector('[data-all-clear]');
-const deleteButton = document.querySelector('[data-delete]');
-const equalsButton = document.querySelector('[data-equals]');
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const previousOperandTextElement = document.querySelector(
-  '.calculator__previous-operand'
-);
-const currentOperandTextElement = document.querySelector(
-  '.calculator__current-operand'
-);
+const body = document.querySelector('body');
+const audio = document.querySelector('.audio');
+
+body.addEventListener('click', (e) => {
+  const buttet = document.createElement('span');
+  buttet.className = 'bullet';
+  let x = e.offsetX;
+  let y = e.offsetY;
+  buttet.style.left = x + 'px';
+  buttet.style.top = y + 'px';
+  body.appendChild(buttet);
+
+  setTimeout(function () {
+    audio.play();
+
+    setTimeout(function () {
+      audio.pause();
+      audio.currentTime = 0;
+    }, 1000);
+  }, 0);
+});
